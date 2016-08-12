35a002c93411e544e9d68bb80228b0b318eb51de
unknown <dande@psq-dande.psqad.com>
2016-08-12 10:25:45 +0530

    Initial commit

---
+++ app/src/main/java/com/peppersquare/dande/feedreader/FeedAdapter.kt
@@ -0,0 +1,30 @@
+package com.peppersquare.dande.feedreader
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by dande on 11-08-2016.
+ */
+class FeedAdapter : RecyclerView.Adapter<FeedAdapter.FeedHolder>() {
+    override fun getItemCount(): Int {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ? , position: Int) {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int):  {
+        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+
+    }
+
+    class FeedHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+
+
+    }
+
+}
